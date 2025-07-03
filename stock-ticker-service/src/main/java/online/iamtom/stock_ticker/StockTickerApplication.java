7f841608205fe824d1c70d7d35bb50403a53b85c
Tom <github@iamtom.online>
2025-07-03 17:44:42 +0100

    Initial commit

---
+++ stock-ticker-service/src/main/java/online/iamtom/stock_ticker/StockTickerApplication.java
@@ -0,0 +1,13 @@
+package online.iamtom.stock_ticker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockTickerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockTickerApplication.class, args);
+	}
+
+}
---
+++ stock-ticker-service/src/test/java/online/iamtom/stock_ticker/StockTickerApplicationTests.java
@@ -0,0 +1,13 @@
+package online.iamtom.stock_ticker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockTickerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
